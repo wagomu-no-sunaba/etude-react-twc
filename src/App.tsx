e6bbcbd2791af6d12914baefa646e03a15037517
staticWagomU <to6wa17@gmail.com>
2024-06-07 22:18:43 +0900

    feat: add card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Card } from './card'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -17,6 +18,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Card>Hello world!</Card>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/card.tsx
@@ -0,0 +1,3 @@
+import { twc } from "react-twc";
+
+export const Card = twc.div`rounded-lg border bg-slate-100 text-black shadow-sm`;
